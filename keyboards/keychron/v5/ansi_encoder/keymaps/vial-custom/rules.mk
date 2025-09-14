4bd49251048030fd0052a6554ccbcf006333e191
BiggieCheese699 <qmk@BIGGIE.localdomain>
2025-09-14 22:44:51 +0300

    Adding custom keymap and editing existing files

---
+++ keyboards/keychron/v5/ansi_encoder/keymaps/vial-custom/rules.mk
@@ -0,0 +1,12 @@
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+VIALRGB_ENABLE = yes
+DEBOUNCE_TYPE = asym_eager_defer_pk
+DYNAMIC_MACRO_ENABLE = yes
+CAPS_WORD_ENABLE = yes
+COMBO_ENABLE = yes
+DIP_SWITCH_MAP_ENABLE= yes
+ENCODER_MAP_ENABLE = yes
+
+VPATH += keyboards/keychron/common
+SRC += keychron_common.c
